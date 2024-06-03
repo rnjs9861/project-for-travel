6aaf17fc69d7af1bbad249a350cbba24091e4955
rnjs9861 <rkwkvlrmakfh@gmail.com>
2024-06-03 23:07:03 +0900

    Docs: 6/03 src/common 폴더 생성 및 common.css 적용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./App.css";
+import "./css/reset.css";
+import "./css/common.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import TestPage from "./pages/TestPage";
 
--- src/pages/TestPage.js
+++ src/pages/TestPage.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Buttons from "../components/common/Button";
 
 const TestPage = () => {
-  return <div>test</div>;
+  return (
+    <>
+      <div>test</div>
+      <Buttons>버튼입니다.</Buttons>
+    </>
+  );
 };
 
 export default TestPage;
